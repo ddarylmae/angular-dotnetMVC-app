717c32c9a74a187ed81896b6cb393f1cf9dc9da2
ddarylmae <diieemmobee@gmail.com>
2018-05-30 13:52:26 +1200

    Modified User table, updated edmx

--- ReadingDiary/Models/User.cs
+++ ReadingDiary/Models/User.cs
@@ -17,5 +17,8 @@ namespace ReadingDiary.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
     }
 }
